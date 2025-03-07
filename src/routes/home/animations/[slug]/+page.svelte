47c91d7cbf49b25ffc194fbf904c468b15908b2e
uchkunovdaniel <du98130933@edu.mon.bg>
2025-03-07 08:15:03 +0200

    Add page

--- src/routes/home/animations/[slug]/+page.svelte
+++ src/routes/home/animations/[slug]/+page.svelte
@@ -2,6 +2,10 @@
     let { data } = $props();
 </script>
 
-<video controls src="{data.video}">
+<video class="w-11/12 h-fit rounded-md m-auto " controls src="{data.video}">
     <track src="" kind="captions">
 </video>
+
+<div class="flex w-screen h-2 bg-(--main) mt-10"></div>
+
+<p class="font-transforma_light text-2xl text-justify p-3">{data.description}</p>

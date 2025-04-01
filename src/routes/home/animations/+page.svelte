4dbe45cafd84af71b6b058930b5ae4afc551d06d
uchkunovdaniel <178625110+uchkunovdaniel@users.noreply.github.com>
2025-04-01 10:44:29 +0300

    Fix

--- src/routes/home/animations/+page.svelte
+++ src/routes/home/animations/+page.svelte
@@ -4,7 +4,7 @@
 
 <h1 class="font-transforma text-3xl text-white fixed top-5 left-5 z-[51]">Aнимации</h1>
 
-<div class="overflow-y-scroll w-screen h-screen flex justify-center items-center flex-col gap-4 mb-10">
+<div class="overflow-y-scroll w-screen h-screen flex justify-start items-center flex-col gap-4 pb-24">
     {#each data.animations as animation, i}
         <button class="w-11/12 h-fit flex justify-center" onclick={() => {window.location.href = `/home/animations/${animation.video[0]}`}} aria-label={animation.video[0]}>
             <img class="rounded-md w-11/12" src={data.thumbnails[i]} alt="animation">
